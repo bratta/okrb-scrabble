8ef90f09cb3183b60c0e10489408ba5de1b26327
Tim Gourley <tgourley@gmail.com>
2011-06-09 18:58:19 -0500

    Initial commit

---
+++ spec/scrabble_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper.rb'
+
+describe "Scrabble" do
+  pending "Put some tests here, yo!"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+
+require File.expand_path("scrabble.rb")
+require 'rspec'
+
+RSpec.configure do |config|
+end
