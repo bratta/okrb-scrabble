8daabca3c8bfe57230a1293ca12b1a663c402726
Tim Gourley <tgourley@gmail.com>
2011-06-09 19:55:07 -0500

    Got the highest scoring word

--- scrabble.rb
+++ scrabble.rb
@@ -1,3 +1,50 @@
 require "rubygems"
 require "json"
 
+class Scrabble
+  attr_accessor :config, :tiles_on_hand
+  
+  def initialize  
+    @config = JSON.parse(File.read("input.json")) 
+    @tiles_on_hand = randomly_generate_tiles   
+  end
+      
+  def get_word_from_tiles
+    valid_words = {}
+    letters = []
+    scores = []
+    @tiles_on_hand.each do |tile|
+      tile.match(/^(\w)(\d+)$/) do |m|
+        letters << m[1]
+        scores << m[2]
+      end
+    end
+    letters.permutation.to_a.each do |permutation|
+      word = permutation.join("")
+      valid_words[word] = score(word) if @config["dictionary"].include?(word)
+    end
+    valid_words.sort_by {|k,v| v}.last[0]
+  end
+  
+  private  
+  
+  def score(word)
+    score = 0
+    word.split(//).each do |letter|
+      @config["tiles"].each do |tile|
+        tile.match(/^#{letter}(\d+)$/) do |m|
+          score += m[1].to_i
+        end
+      end
+    end
+    score
+  end
+  
+  def randomly_generate_tiles
+    tiles = []
+    9.times do
+      tiles << @config["tiles"][rand(@config["tiles"].count)]
+    end
+    tiles
+  end
+end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper.rb'
 
 describe "Scrabble" do
-  pending "Put some tests here, yo!"
+  before do
+    @scrabble = Scrabble.new
+    @output = <<EOF
+1 1 1 1 1 1 1 1 1 1 1 1
+1 1 1 2 1 2 1 1 1 1 1 1
+1 2 w h i f f l i n g 1
+2 1 1 1 1 1 1 1 1 2 2 1
+1 1 1 2 1 1 1 1 1 1 1 1
+1 1 1 1 1 1 2 1 1 1 2 1
+1 1 1 1 1 1 1 1 2 1 1 1
+1 1 1 1 1 1 1 1 1 1 1 2
+1 1 1 1 1 1 1 1 1 1 1 1
+EOF
+    @scrabble.tiles_on_hand = [ "w5", "h8", "i4", "f7", "f7", "l3", "i4", "n1", "g6" ]
+  end
+  
+  it "loads the input data" do
+    @scrabble.config.should_not be_empty
+  end
+  
+  it "generates random tiles" do
+    @scrabble.tiles_on_hand.count.should == 9
+  end
+  
+  it "gets highest scoring word from the tiles on hand" do
+    @scrabble.get_word_from_tiles.should == "whiffling"
+  end
+  
+  it "shows the board with the most optimal first move" do
+    #@scrabble.tiles_on_hand = [ "w5", "h8", "i4", "f7", "f7", "l3", "i4", "n1", "g6" ]
+    #@scrabble.output_first_move.should == @output
+  end
 end
